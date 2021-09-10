89409834f56fd351111568f1781523337393e21e
thecommonnoob <lepaysdespoemes@gmail.com>
2021-09-10 18:58:52 +0200

    Fixed some formatting

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-number = input("Input a decimal number with the repeating digits in between brackets: ")
+number = input('Input a decimal number with the repeating digits in between brackets: ')
 
 
 def parse_input(input):
     repeating_number = input.split('(', 1)[1].split(')')[0]
-    original_number = input.replace("(", "")
-    original_number = original_number.replace(")", "")
-    return repeating_number, original_number
+    original_number = input.replace('(', '')
+    original_number = original_number.replace(')', '')
+    return (repeating_number, original_number)
 
 
 def do_math(repeating_number, original_number):
+
     # does like thing idk forgot what i did
+
     multiplied_number = original_number
     decimal_thing = repeating_number
     multiplied_by = 1
@@ -19,13 +21,16 @@ def do_math(repeating_number, original_number):
         multiplied_by *= 10
 
     # not at all useless
+
     multiplied_number += decimal_thing
 
     # uh do the minus thing
+
     multiplied_number -= decimal_thing
     multiplied_by -= 1
 
     # fractions woo
+
     top_thingy = int(multiplied_number)
     bottom_thingy = int(multiplied_by)
 
@@ -42,7 +47,5 @@ def do_math(repeating_number, original_number):
 
     return (top_thingy, bottom_thingy)
 
-
-# print(parse_input(number)[1])
 
 print(do_math(parse_input(number)[0], parse_input(number)[1]))
